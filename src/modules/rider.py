c219f58f22a504437830b764b507eae3b0762d20
Minh Pham <phamduya@sheridancollege.ca>
2025-07-14 00:31:31 -0400

    Create location class

--- src/modules/rider.py
+++ src/modules/rider.py
@@ -1,13 +1,11 @@
 from typing import List
 from .user import User
+from src.utils.location import Location
 
 class Rider(User):
-    def __init__(self, user_id: str, email: str, user_name: str, longitude: float = 0.0, latitude: float = 0.0):
+    def __init__(self, user_id: str, email: str, user_name: str, longitude: float, latitude: float):
         super().__init__(user_id, email, user_name)
-        if (-180 <= longitude <= 180) and (-90 <= latitude <= 90):
-            self.current_location = (latitude, longitude)
-        else:
-            raise ValueError("Invalid coordinates: Longitude must be between -180 and 180, Latitude must be between -90 and 90.")
+        self.current_location = Location(latitude, longitude)
         self.current_ride = None
         self.ride_history: List = [] 
     
