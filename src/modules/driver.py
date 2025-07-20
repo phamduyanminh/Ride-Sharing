1808c64234b586c6644209b22a7fa57cdf7ec275
Minh Pham <phamduya@sheridancollege.ca>
2025-07-19 21:37:18 -0400

    Add accept ride and cancel ride functions for driver

--- src/modules/driver.py
+++ src/modules/driver.py
@@ -1,11 +1,32 @@
 from typing import List
 from .user import User
 from src.utils.location import Location
+from src.utils.ride import Ride
 
 class Driver(User):
     def __init__(self, user_id: str, email: str, user_name: str, longitude: float, latitude: float):
         super().__init__(user_id, email, user_name) 
+        self.current_location: Location = Location(latitude, longitude)
+        self.is_available: bool = True
+        self.current_ride: Ride = None
+        self.drive_history: List[Ride] = [] 
+    
+    def update_location(self, latitude, longitude):
         self.current_location = Location(latitude, longitude)
-        self.is_available = True
+        print(f"Driver {self.user_name} location updated to {self.current_location}.")
+    
+    def accept_ride(self, ride):
+        if not self.is_available:
+            raise Exception("Driver is not available to accept a new ride.")
+        
+        self.current_ride = ride
+        self.is_available = False
+        print(f"Driver {self.user_name} has accepted a ride from {ride.start_location} to {ride.end_location}.")
+    
+    def cancel_ride(self, ride):
+        if self.current_ride is None:
+            raise Exception("No current ride to cancel.")
+        
         self.current_ride = None
-        self.drive_history: List = [] 
+        self.is_available = True
+        print(f"Driver {self.user_name} has cancelled a ride from {ride.start_location} to {ride.end_location}.")
